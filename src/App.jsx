ea89381b5437fe3a4c2bbc1a4e3ce43f44e1dfd7
Manimadhav <manimadhav2@gmail.com>
2025-11-10 12:48:42 +0530

    feat(app): Added fetch suggestions, rendering options

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React,{useRef} from "react";
 import "./App.css";
 import AutoComplete from "./components/AutoComplete";
 
 function App() {
+
+  const abortRef = useRef(null)
+
+  const fetchSuggestions = async(query) => {
+    if(abortRef.current){
+      abortRef.current.abort()
+    }
+    const controller = new AbortController()
+    abortRef.current = controller
+    const URL = `https://dummyjson.com/recipes/search?q=${query}`
+    try{
+      const result = await fetch(URL,{signal:controller.signal})
+      if(!result.ok){
+        throw new Error('Failed !!')
+      }
+      const response = await result.json()
+      return response.recipes
+    }catch(err){
+      if(err.name === 'AbortError'){
+        return []
+      }
+      throw err
+    }
+  }
+
   return (
     <>
       <AutoComplete
         onChange={() => {}}
         onBlur={() => {}}
         onFocus={() => {}}
-        fetchSuggestions={() => {}}
+        fetchSuggestions={fetchSuggestions}
         onSelect={() => {}}
         placeHolder={"Type to search ..."}
         customStyles={null}
--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const AutoComplete = ({
   onChange = () => {},
@@ -13,12 +13,43 @@ const AutoComplete = ({
   staticData,
 }) => {
   const [inputValue, setInputValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleInputChange = (e) => {
     const userValue = e.target.value;
     setInputValue(userValue);
     onChange(userValue);
   };
+
+  const getSuggestions = async (query) => {
+    if (!query) {
+      setSuggestions([]);
+    }
+    setError(null);
+    setLoading(true);
+    try {
+      let result;
+      if (staticData) {
+        result = staticData.filter((item) => {
+          return item.toLowerCase().includes(query.toLowerCase());
+        });
+      } else if (fetchSuggestions) {
+        result = await fetchSuggestions(query);
+      }
+      setSuggestions(result);
+    } catch (err) {
+      setError("Failed to fetch suggestions");
+      setSuggestions([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getSuggestions(inputValue);
+  }, [inputValue]);
 
   return (
     <div style={{ position: "relative", width: "300px" }}>
@@ -36,6 +67,46 @@ const AutoComplete = ({
           ...customStyles,
         }}
       />
+      {loading && <>{customLoading}</>}
+      {error && <div>{error}</div>}
+      <div
+        style={{
+          margin: "0",
+          padding: "10px",
+          maxHeight: "200px",
+          overflowY: "scroll",
+          width: "100%",
+          border: "1px solid #ccc",
+          borderRadius: "4px",
+          borderTop: "none",
+        }}
+      >
+        {suggestions &&
+          suggestions.map((suggestion) => {
+            let currentSuggestion;
+            if (dataKey) {
+              currentSuggestion = suggestion[dataKey];
+            } else if (typeof suggestion === "object") {
+              currentSuggestion =
+                suggestion.name || suggestion.id || JSON.stringify(suggestion);
+            } else {
+              currentSuggestion = suggestion;
+            }
+            return (
+              <div
+                style={{
+                  padding: "10px",
+                  cursor: "pointer",
+                  width: "100%",
+                  borderTop: "none",
+                }}
+                key={currentSuggestion}
+              >
+                {currentSuggestion}
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
