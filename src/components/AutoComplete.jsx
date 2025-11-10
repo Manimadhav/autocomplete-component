28289e9f83b83d40385f71a873cd98cd819b88c9
Manimadhav <manimadhav2@gmail.com>
2025-11-10 11:53:03 +0530

    feat(app): Added user input for search

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AutoComplete = ({
   onChange = () => {},
@@ -12,7 +12,32 @@ const AutoComplete = ({
   customLoading,
   staticData,
 }) => {
-  return <div>AutoComplete</div>
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    const userValue = e.target.value;
+    setInputValue(userValue);
+    onChange(userValue);
+  };
+
+  return (
+    <div style={{ position: "relative", width: "300px" }}>
+      <input
+        value={inputValue}
+        onChange={(e) => handleInputChange(e)}
+        placeholder={placeHolder}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        style={{
+          width: "100%",
+          padding: "10px",
+          border: "1px solid #ccc",
+          borderRadius: "4px",
+          ...customStyles,
+        }}
+      />
+    </div>
+  );
 };
 
 export default AutoComplete;
