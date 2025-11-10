e719f4b405612b51b91923ac6018193ce527f869
Manimadhav <manimadhav2@gmail.com>
2025-11-10 11:45:39 +0530

    feat(app): integrated AutoComplete component into main app

---
+++ src/components/AutoComplete.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AutoComplete = ({
+  onChange = () => {},
+  onBlur = () => {},
+  onFocus = () => {},
+  fetchSuggestions = () => {},
+  onSelect = () => {},
+  placeHolder = "",
+  customStyles,
+  dataKey,
+  customLoading,
+  staticData,
+}) => {
+  return <div>AutoComplete</div>
+};
+
+export default AutoComplete;
